4041ffd838bd0a3b3d3ddaacb9f810d886e19678
elias <elias.gauthier@etu.u-bordeaux.fr>
2026-01-13 19:23:11 +0100

    Try to fix pipeline

--- examples/rotation.ml
+++ examples/rotation.ml
@@ -4,8 +4,7 @@ let q = Qubit.zero ()
 
 let rotation q =
   (* 30° rotation then 45° rotation *)
-  Gate.ry q (3.14 /. 6.0);
-  Gate.rx q (3.14 /. 4.0);
+  Gate.rx q (3.14);
   Visualization.plot_bloch q ()
 
 let () =
