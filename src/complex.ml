3da115e25933285155e5b39eb1e30a70c8293649
Elias <79870068+elias-utf8@users.noreply.github.com>
2026-01-16 15:41:31 +0100

    Update copyright year in complex.ml

--- src/complex.ml
+++ src/complex.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright 2025 Elias GAUTHIER <elias.gauthier@etu.u-bordeaux.fr>
+ * Copyright 2026 Elias GAUTHIER <elias.gauthier@etu.u-bordeaux.fr>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
