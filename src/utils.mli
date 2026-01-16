382ab605f87c1b16d4743f9563ca7663cb61e491
elias <elias.gauthier@justice.fr>
2026-01-16 15:04:42 +0100

    Add somes comments

--- src/utils.mli
+++ src/utils.mli
@@ -1,1 +1,14 @@
+(**
+  @author       Elias GAUTHIER
+  file :        utils.mli
+  date :        2026-16-01
+  license :     Apache-2.0
+  description : Various utility functions for qcaml.
+*)
+
+(** Convert int in binary string reprensation
+
+    @param n Integer to convert
+    @return String binary representation
+*)
 val strbin_of_int : int -> string
